a34ae9d42913a4ea30aab52061f9d8155916ad8c
vcyt111111 <35905056+vcyt111111@users.noreply.github.com>
2018-03-08 21:13:00 -0500

    Work Up to lab 2

---
+++ Test 3/Assets/ClickHandler1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler1 : MonoBehaviour {
+    public GameObject myT_shirt;
+    private Rigidbody2D rBody;
+    private SpriteRenderer sRend;
+
+	// Use this for initialization
+	void Start () {
+        rBody = myT_shirt.GetComponent<Rigidbody2D>();
+        sRend = myT_shirt.GetComponent<SpriteRenderer>();
+    }
+
+    void OnMouseDown()
+    {
+        sRend.color = Color.blue;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Test 3/Assets/ClickHandler2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickHandler2 : MonoBehaviour {
+    public GameObject myT_shirt;
+    private Rigidbody2D rBody;
+    private SpriteRenderer sRend;
+
+	// Use this for initialization
+	void Start () {
+        rBody = myT_shirt.GetComponent<Rigidbody2D>();
+        sRend = myT_shirt.GetComponent<SpriteRenderer>();
+	}
+    void OnMouseDown()
+    {
+        sRend.color = Color.red;
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
